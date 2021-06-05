366d8567651841cb7dc568e457bb872cd81bdc88
Markus Skov <Markusskov@hotmail.no>
2021-06-05 02:27:58 +0200

    Turned homepage in to one-page with multiple sections.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
+document.querySelector('.fa-user').onclick = function () {
+  document.querySelector('.fa-user').style.color = 'var(--primary-color)';
+  document.querySelector('.fa-graduation-cap').style.color = '#868686';
+  document.querySelector('.fa-briefcase').style.color = '#868686';
+  document.querySelector('.work').style.display = 'none';
+  document.querySelector('.education').style.display = 'none';
+  document.querySelector('.blueprofile__image').style.display = 'block';
+  document.querySelector('.profile').style.display = 'flex';
+  document.querySelector('.red__image').style.display = 'none';
+};
+
 document.querySelector('.fa-graduation-cap').onclick = function () {
   document.querySelector('.fa-graduation-cap').style.color =
     'var(--primary-color)';
   document.querySelector('.fa-user').style.color = '#868686';
   document.querySelector('.fa-briefcase').style.color = '#868686';
-};
-
-document.querySelector('.fa-user').onclick = function () {
-  document.querySelector('.fa-user').style.color = 'var(--primary-color)';
-  document.querySelector('.fa-graduation-cap').style.color = '#868686';
-  document.querySelector('.fa-briefcase').style.color = '#868686';
+  document.querySelector('.profile').style.display = 'none';
+  document.querySelector('.work').style.display = 'none';
+  document.querySelector('.blueprofile__image').style.display = 'none';
+  document.querySelector('.education').style.display = 'flex';
+  document.querySelector('.red__image').style.display = 'block';
 };
 
 document.querySelector('.fa-briefcase').onclick = function () {
   document.querySelector('.fa-briefcase').style.color = 'var(--primary-color)';
   document.querySelector('.fa-graduation-cap').style.color = '#868686';
   document.querySelector('.fa-user').style.color = '#868686';
+  document.querySelector('.profile').style.display = 'none';
+  document.querySelector('.education').style.display = 'none';
+  document.querySelector('.blueprofile__image').style.display = 'none';
+  document.querySelector('.work').style.display = 'flex';
+  document.querySelector('.red__image').style.display = 'none';
 };
